a97e37bc8638d404c658f85b80a312c0d0d79638
Roger Araque <raismaelr@gmail.com>
2017-05-23 13:16:54 -0400

    Login Funcionando, se recibe el token del API /security/login, en angular estamos usando el factory, services y controller. Este commit es por si algo sale mal en las proximas modificaciones y tengamos que volver a él

--- src/main/java/com/wd/security/component/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/wd/security/component/JwtAuthenticationEntryPoint.java
@@ -29,7 +29,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 		 */
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "NO AUTORIZADO");
 	
-	
+		
+		
 	}
 
 }
